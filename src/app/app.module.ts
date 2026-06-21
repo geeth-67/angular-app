f9c318575e8ab51e3683003b78e363c28f353dcc
vishwageethanjana <geeth.me67@gmail.com>
2026-06-21 09:51:16 +0530

    add angular module import & export

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import {ProductModule} from "./modules/product/product.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    SideBarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
