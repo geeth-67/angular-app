8565976ff425f66b85136ec2bfca87c2af1a1f74
vishwageethanjana <geeth.me67@gmail.com>
2026-06-21 09:51:31 +0530

    add angular module import & export

---
+++ src/app/modules/product/product.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './components/product/product.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductComponent
+  ],
+  exports: [
+    ProductComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductModule { }
